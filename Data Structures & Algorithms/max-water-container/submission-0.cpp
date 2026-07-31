7be5afe8c439519847f57d432d7bb86965147e63
Colin Stack <64753691+colinstick@users.noreply.github.com>
2026-07-31 18:00:53 -0500

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        int l = 0; int r = heights.size()-1;
+        int ans = 0;
+        while(l < r) {
+            // calc curr
+            ans = max(ans, (r-l)*min(heights[l],heights[r]));
+            if(heights[l] < heights[r]) l++;
+            else r--;
+        }
+        return ans;
+    }
+};
